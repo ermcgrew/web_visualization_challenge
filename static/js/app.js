414c406568a403732058085fa13430a678ce0d40
PlanetM <ermcgrew@users.noreply.github.com>
2021-12-31 11:24:26 -0500

    first draft of horizontal bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -6,30 +6,39 @@ async function main() {
     const data = await response.json();
 
     //test
-    console.log(data.metadata[0]); //access metadata
-    console.log(data.samples[0]);
+    // console.log(data.metadata[0]); //access metadata
+    // console.log(data.samples[0]); //access sample info
 
 
-    //arrays of data
+    //arrays of data for initial load, already sorted by descending sample_values
+    let sample_values = Object.values(data.samples[0].sample_values).slice(0,10);
+    // console.log(sample_values);
+    let otu_ids = Object.values(data.samples[0].otu_ids).slice(0,10);
+  
+    //add "OTU" to beginning of otu id numbers
+    for (let i = 0; i < otu_ids.length; i++){
+        otu_ids[i] = "OTU " + otu_ids[i];
+    };
+    // console.log(otu_ids);
 
+    let otu_labels = Object.values(data.samples[0].otu_labels).slice(0,10);
+    // console.log(otu_labels);
 
 
     //Trace 1, horizontal bar chart (dropdown menu?) of top 10 OTUs/individual
-    //Use sample_values as the values for the bar chart.
-    //Use otu_ids as the labels for the bar chart.
-    //Use otu_labels as the hovertext for the chart.
-    // let trace = [{
-    //     values: , 
-    //     labels: , 
-    //     type: 'bar',
-    //     orientation: 'h' 
-    // }];
+    let trace = [{
+        x: sample_values, 
+        y: otu_ids,
+        text: otu_labels,
+        type: 'bar',
+        orientation: 'h' 
+    }];
 
     // const layout = {
         
     // }; 
 
-    // Plotly.newPlot('bar', trace, layout);
+    Plotly.newPlot('bar', trace);//, layout);
 
     //Trace 2, Bubble chart of each sample
     // Use otu_ids for the x values.
