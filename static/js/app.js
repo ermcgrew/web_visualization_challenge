13cb497f0a2524eda178153a688ef0c75155269c
PlanetM <ermcgrew@users.noreply.github.com>
2022-01-04 10:58:42 -0500

    selection change working!

--- static/js/app.js
+++ static/js/app.js
@@ -5,49 +5,93 @@ async function optionChanged(valueSel) {
     const response = await fetch("./samples.json");
     const data = await response.json();
 
-    //************************************************************* */
-    //if a selection from the drop-down is made:
-    if (valueSel){
-        console.log(valueSel);
-        console.log(data.samples[0].id);
-                
-        let nameList = data.names;
-
-        let selection = nameList.filter(name => name === valueSel);//always gives 0, array is one item long
-        console.log(selection);
-        //find matching sample id
-        //need the index number  
-        // let selection = names.find(name => name === valueSel);
-        // .filter
-
-        // let indexSel = 
-        // data.samples[].id
-        // data.samples is an Array of objects
-
-        // [
-        //     {id:904 (key:value),
-
-        //     },
-        //     {
-
-        //     },
-        // ]
+    //populate drop-down with sample ID names
+    let names = Object.values(data.names); 
+    for (i=0; i<names.length; i++) {
+        //set create methods as variables (have to do this inside the loop)
+        const newOption = document.createElement('option');
+        const attributeVal = document.createAttribute('value');
         
-        //update arrays for:
-            // let sample_values = Object.values(data.samples[0].sample_values);
-            // let otu_labels = Object.values(data.samples[0].otu_labels);
-            // let otu_ids = Object.values(data.samples[0].otu_ids);
-            // //add "OTU" to beginning of otu id numbers for trace1
-            // let otu_ids_labeled = otu_ids.map(id => "OTU " + id);
-            // let metadataStrings = (Object.entries(data.metadata[0]))
-                //.map(item => `${item[0]}: ${item[1]}`);
+        //set display text and value to sample id name
+        newOption.textContent = names[i];
+        attributeVal.value = names[i];
         
-            //two plotly updates
+        //add newOption
+        document.querySelector("#selDataset").append(newOption);
+        //add value attribute 
+        newOption.setAttributeNode(attributeVal);
+    }; 
+
+
+    //declare variable to hold index # outside for/if loop
+    let indexSel = 0;
+
+    if (valueSel) {  //if a selection from the drop-down is made
+        // console.log(valueSel);
+        //find matching sample id index number    
+        for (let i=0; i<names.length;i++) {
+            if (data.samples[i].id === valueSel){
+                indexSel = i;
+                // console.log(indexSel);
+            }
+        };
+
+        //update arrays with new sample:
+        let sample_values = Object.values(data.samples[indexSel].sample_values);
+        let otu_labels = Object.values(data.samples[indexSel].otu_labels);
+        let otu_ids = Object.values(data.samples[indexSel].otu_ids);
+        //add "OTU" to beginning of otu id numbers for trace1
+        let otu_ids_labeled = otu_ids.map(id => "OTU " + id);
+        let metadataStrings = (Object.entries(data.metadata[indexSel]))
+            .map(item => `${item[0]}: ${item[1]}`);
         
-            //update metadata
+
+        //two plotly updates
+        let trace1 = [{
+            x: sample_values.slice(0,10).reverse(), 
+            y: otu_ids_labeled.slice(0,10).reverse(),
+            text: otu_labels.slice(0,10).reverse(),
+            type: 'bar',
+            orientation: 'h' 
+        }];
+    
+        const layout1 = {
+            title: '10 Most Frequently Occuring OTUs'
+        }; 
+    
+        Plotly.newPlot('bar', trace1, layout1);
+
+        //Trace 2, Bubble chart of each sample
+        let trace2 =[{
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: 'markers',
+            marker: {
+            color: otu_ids,
+            size: sample_values
+            }
+        }];
+
+        let layout2 = {
+            title: 'Number of samples per OTU',
+            xaxis: {
+            title: 'OTU ID',
+            }, 
+        };
+        
+        Plotly.newPlot('bubble', trace2, layout2);
+           
+        //update metadata
+        metadataStrings.map(item => {
+            let newP = document.createElement('p');
+            newP.textContent = item;
+            document.querySelector('.panel-body').append(item);
+            document.querySelector('.panel-body').append(document.createElement('br')); //line break for next element
+        });
     
     }
-    else {
+    else { 
         //arrays of data for initial load, already sorted by descending sample_values
         let sample_values = Object.values(data.samples[0].sample_values);
         let otu_labels = Object.values(data.samples[0].otu_labels);
@@ -81,7 +125,7 @@ async function optionChanged(valueSel) {
             text: otu_labels,
             mode: 'markers',
             marker: {
-            color: otu_ids, //change color scheme?
+            color: otu_ids,
             size: sample_values
             }
         }];
@@ -102,29 +146,15 @@ async function optionChanged(valueSel) {
             .map(item => `${item[0]}: ${item[1]}`);
 
         //add to element with class="panel-body"
-        let newP = document.createElement('p');
         metadataStrings.map(item => {
-            document.querySelector('.panel-body').append(item, newP);
+            let newP = document.createElement('p');
+            newP.textContent = item;
+            document.querySelector('.panel-body').append(item);
             document.querySelector('.panel-body').append(document.createElement('br')); //line break for next element
         });
     };
 
-    //populate drop-down with sample ID names
-    let names = Object.values(data.names); 
-    for (i=0; i<names.length; i++) {
-        //set create methods as variables (have to do this inside the loop)
-        const newOption = document.createElement('option');
-        const attributeVal = document.createAttribute('value');
-        
-        //set display text and value to sample id name
-        newOption.textContent = names[i];
-        attributeVal.value = names[i];
-        
-        //add newOption
-        document.querySelector("#selDataset").append(newOption);
-        //add value attribute 
-        newOption.setAttributeNode(attributeVal);
-    }; 
+    
 
 };
 
